c34b9201b5011c23512f3bce6dc742ed1f775f19
stormwine1986 <47297412+stormwine1986@users.noreply.github.com>
2020-11-19 21:39:29 +0800

    移动日志配置文件

--- ntp/src/main/java/org/pjia/wrvs/plugins/ntp/App.java
+++ ntp/src/main/java/org/pjia/wrvs/plugins/ntp/App.java
@@ -1,7 +1,11 @@
 package org.pjia.wrvs.plugins.ntp;
 
+import java.util.Arrays;
+
 import org.pjia.wrvs.plugins.client.PluginApp;
 import org.pjia.wrvs.plugins.client.PluginContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * App
@@ -9,15 +13,20 @@ import org.pjia.wrvs.plugins.client.PluginContext;
  * @author pjia
  *
  */
+@Slf4j
 public class App implements PluginApp {
 	
 	@Override
     public void run(String[] args, PluginContext context) {
-    	String appName = args[0];
-    	if("import".equals(appName)) {
-    		ImportApp.run(args, context);
-    	}else if("export".equals(appName)) {
-    		ExportApp.run(args, context);
-    	}
+		log.debug("args = " + Arrays.toString(args));
+		if(args.length > 0) {
+			String appName = args[0];
+			log.debug("appName = " + appName);
+			if("import".equals(appName)) {
+				ImportApp.run(args, context);
+			}else if("export".equals(appName)) {
+				ExportApp.run(args, context);
+			}			
+		}
     }
 }
